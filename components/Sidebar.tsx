c6b90f357b4e3894b989ab2e383636fad4cfde23
Noman Sipai <122717436+NomanSipai@users.noreply.github.com>
2024-12-11 16:54:47 +0530

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+const Sidebar = () => {
+  const pathname = usePathname();
+  return (
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map(({ label, route, imgUrl }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+          return (
+            <Link
+              key={label}
+              href={route}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              <Image src={imgUrl} alt={label} width={24} height={24} />
+              <p className="text-lg font-semibold max-lg:hidden">{label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
